590b0726bc2b50d74cea955df742e661bcbee6dd
hroun20182 <harounrachid0000@gmail.com>
2025-05-10 21:06:57 +0100

    Update check_file_exist.sh

--- check_file_exist.sh
+++ check_file_exist.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
+#Demander à l'utilisateur le nom du fichier
 echo "Entrer le nom du fichier :" 
 read "filename"
 #verifie si le fichier existe
 if [ -f "$filename" ];
  then
-echo "Le fichier  existe."
+echo "Le fichier '$filename' existe."
 else
-echo "Le fichier  n'existe pas."
+echo "Le fichier '$filename' n'existe pas."
 fi
